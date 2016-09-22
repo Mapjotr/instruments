3d2101f7d1d67f4aa0d48e154191a28cfd00973a
Mapjotr <peter.schmiz@gmx.de>
2016-09-22 08:04:07 +0200

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+    	log_in @user
         flash[:success] = "Die Anmeldung war erfolgreich :-) Willkommen im Club!"
         redirect_to @user
     else
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,25 +4,29 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "invalid signup information" do
     get signup_path
+    assert_select 'form[action="/signup"]'
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+      post signup_path, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
   end
 
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count' do
-      post users_path, params: { user: { name:  "Brad Tester",
+      post signup_path, params: { user: { name:  "Brad Tester",
                                          email: "test@example.com",
                                          password:    			"password",
                                          password_confirmation: "password" } }
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
   end
 
 end
