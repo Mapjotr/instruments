fee71db74f4ceff7bdb3cb929239c485423e5cbe
Mapjotr <peter.schmiz@gmx.de>
2017-01-28 11:50:42 +0100

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+    def setup 
+        @user = users(:michael) 
+    end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -14,6 +18,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Kontakt")
     get signup_path
     assert_select "title", full_title("Anmeldung")
+    assert_select "a[href=?]", users_path, count: 0 #make sure that there is no link to users_path when not logged in
+    log_in_as(@user)
+    assert_redirected_to user_url(@user)
+    follow_redirect!
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", users_path
   end
 
 end
